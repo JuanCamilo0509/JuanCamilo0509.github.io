81903f6c1373a16e9929011dde048eb641a355e5
Juan Camilo <juank109845@gmail.com>
2022-05-14 15:48:38 -0500

    Actualizacion3.6

--- Clave.js
+++ Clave.js
@@ -1,7 +1,8 @@
 const confirmar = document.querySelector(".Confirmar");
 confirmar.addEventListener("click", function () {
 	if (
-		(document.querySelector(".User").value == "luna" || document.querySelector(".User").value == "Luna" || document.querySelector(".User").value == "Luna " || document.querySelector(".User").value == "luna ") &
+		((document.querySelector(".User").value == "luna" || document.querySelector(".User").value == "Luna" || document.querySelector(".User").value == "Luna " || document.querySelector(".User").value == "luna " ) ||(document.querySelector(".User").value == "camilo" || document.querySelector(".User").value == "Camilo" || document.querySelector(".User").value == "Camilo " || document.querySelector(".User").value == "camilo "
+)) &
 		(document.querySelector(".Password").value == "14.9"
 	)) {
 		window.location = "/Main/main.html";
